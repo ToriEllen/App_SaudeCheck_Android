aef7a94929b3f9d9eb64a9ccb4c5939a0841735a
RivailleVD <levyp56@gmail.com>
2026-03-25 19:47:31 -0300

    implementaçao das telas de login e cadastro com a conexao com a api da firebase

--- App.js
+++ App.js
@@ -24,3 +24,14 @@ import AppNavigator from './src/navigation/AppNavigator'
 export default function App() {
   return <AppNavigator />
 }
+/*
+para ativar o kvm
+
+sudo modprobe kvm_amd
+  sudo chmod 666 /dev/kvm
+
+
+
+/*~/Android/Sdk/emulator/emulator -avd NOME_DO_AVD -memory 1024 -no-snapshot &    para iniciar o emulador
+/*npx expo run:android  rodar a aplicaçao
+*/
